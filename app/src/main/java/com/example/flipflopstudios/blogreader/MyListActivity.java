71b0ce960cd05686f81ad70cae73a63d4cec5740
chrisrasch <rasch83@gmail.com>
2014-10-27 17:12:21 -0500

    added string array to strings xml and called new stringArray in the onCreated method

--- app/src/main/java/com/example/flipflopstudios/blogreader/MyListActivity.java
+++ app/src/main/java/com/example/flipflopstudios/blogreader/MyListActivity.java
@@ -1,6 +1,7 @@
 package com.example.flipflopstudios.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,25 +10,17 @@ import android.widget.ArrayAdapter;
 
 public class MyListActivity extends ListActivity {
 
-    protected String[] mAndroidNames =
-            {"Alpha",
-            "Beta",
-            "Cupcake",
-            "Donut",
-            "Eclair",
-            "Froyo",
-            "Gingerbread",
-            "Honeycomb",
-            "Ice Cream Sandwich",
-            "Jelly Bean",
-            "KitKat",
-            "Lollipop",
-};
+    protected String[] mAndroidNames;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
+
+        Resources resources = getResources();
+
+        mAndroidNames = resources.getStringArray(R.array.android_names);
 
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mAndroidNames);
         setListAdapter(adapter);
