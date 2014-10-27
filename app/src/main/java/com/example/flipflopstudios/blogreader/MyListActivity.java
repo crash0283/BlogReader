debfecc2ce24a30ea41abc3af730059a5e8d181b
chrisrasch <rasch83@gmail.com>
2014-10-27 16:54:29 -0500

    added list names

--- app/src/main/java/com/example/flipflopstudios/blogreader/MyListActivity.java
+++ app/src/main/java/com/example/flipflopstudios/blogreader/MyListActivity.java
@@ -4,14 +4,34 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
 public class MyListActivity extends ListActivity {
+
+    protected String[] mAndroidNames =
+            {"Alpha",
+            "Beta",
+            "Cupcake",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mAndroidNames);
+        setListAdapter(adapter);
+
     }
 
 
