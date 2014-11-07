2c7825d64d397330d4d517aed5b4c415b0aeff5e
chrisrasch <rasch83@gmail.com>
2014-11-06 21:17:38 -0600

    almost done

--- app/src/main/java/com/example/flipflopstudios/blogreader/BlogWebViewActivity.java
+++ app/src/main/java/com/example/flipflopstudios/blogreader/BlogWebViewActivity.java
@@ -1,9 +1,12 @@
 package com.example.flipflopstudios.blogreader;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 
 public class BlogWebViewActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class BlogWebViewActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web_view);
+
+        Intent intent = getIntent();
+        Uri blogUri = intent.getData();
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl(blogUri.toString());
+
+
     }
 
 
